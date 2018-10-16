8c7b618e7275582460466de668ba39200e42458d
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-10-16 07:26:57 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,35 @@
 // Clicking start button:
+    // window.onload = function() {
+    //     $("#start").on("click", stopwatch.start);
+    //   };
     // timer appear (decrementing from 30 sec)
+        // reference 09 interval
     // question appears
     // multiple choice answers appear as clickable buttons
 
-// When timer ends:
+// When timer ends: (SetTimeout)
+
+        //  //  The decrement function.
+        //  function decrement() {
+
+        //     //  Decrease number by one.
+        //     number--;
+
+        //     //  Show the number in the #show-number tag. (change this to actual path)
+        //     $("#show-number").html("<h2>" + number + "</h2>");
+
+
+        //     //  Once number hits zero...
+        //     if (number === 0) {
+
+        //       //  ...run the stop function.
+        //       stop();
+
+        //       //  Alert the user that time is up. (change this to show the interum screen)
+        //       alert("Time Up!");
+        //     }
+        //   }
+
     // LOSER message appears
     // Answer to question appears
     // gif of answer appears
@@ -14,7 +40,7 @@
     // Answer to question appears
     // gif of answer appears
 
-// (Interum screen) Screen between questions lasts 3000 ms and then next question is shown  automatically and timer begins to decrease from 30 sec (no user interaction needed)
+// (Interum screen) Screen between questions lasts 3000 ms and then next question is shown automatically and timer begins to decrease from 30 sec (no user interaction needed)
     // reference 11 slide show. Interum screen is the loading image / images will be question screen
 
 
