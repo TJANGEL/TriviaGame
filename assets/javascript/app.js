2c8b2a63d2778ce2a3dca0cf1898a700d90c9cf4
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-10-15 20:31:52 -0400

    JS Pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// Clicking start button:
+    // timer appear (decrementing from 30 sec)
+    // question appears
+    // multiple choice answers appear as clickable buttons
+
+// When timer ends:
+    // LOSER message appears
+    // Answer to question appears
+    // gif of answer appears
+
+// When correct answer is clicked: 
+    // Timer is stopped and reset
+    // WINNER message appears
+    // Answer to question appears
+    // gif of answer appears
+
+// (Interum screen) Screen between questions lasts 3000 ms and then next question is shown  automatically and timer begins to decrease from 30 sec (no user interaction needed)
+    // reference 11 slide show. Interum screen is the loading image / images will be question screen
+
+
+// reset function to start game over / DOES NOT REFRESH THE PAGE
+    // reference 10 stopwatch
